b88b12b0a8eb678ca96bef1855c4c323891b7386
Andres JG <anjimenez11@hotmail.com>
2020-08-17 21:22:17 -0500

    docs

---
+++ docs/precache-manifest.62a2bdd692eb311cc919a6489c34bfa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04bae547c229dea958410d31e6353f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5925ffdef801b6fb0944",
+    "url": "/static/css/main.9073a159.chunk.css"
+  },
+  {
+    "revision": "424e34b960a42fde4046",
+    "url": "/static/js/2.ef86d1b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef86d1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5925ffdef801b6fb0944",
+    "url": "/static/js/main.27a3be7e.chunk.js"
+  },
+  {
+    "revision": "ef0e0127a75cdb20d5ea",
+    "url": "/static/js/runtime-main.183ce351.js"
+  }
+]);
